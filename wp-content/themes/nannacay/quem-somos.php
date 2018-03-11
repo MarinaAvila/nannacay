65abcfadefa08c5769dc57c8abb625a9b58ee891
Marina Avila <marina@grama.cc>
2018-03-11 17:35:54 -0300

    menu-home

--- wp-content/themes/nannacay/quem-somos.php
+++ wp-content/themes/nannacay/quem-somos.php
@@ -4,7 +4,50 @@
   get_header();
 ?>
 
-<h1>quem somos</h1>
+<div class="cover">
+  <h1 class="title">Quem somos</h1>
+</div>
+
+<p class="title">nannacay®</p>
+
+<p class="definition">
+  (Nãnãkay), nome de origem</br>
+  Quechua Aimara, que significa</br>
+  irmandade de mulheres.
+</p>
+
+<div class="image-subtitle">
+  <img class="image" src="">
+  <p class="subtitle">Vivamus id diam sit amet orci porta eleifend sit amet malesuada nisl.</p>
+</div>
+
+<p class="text">
+  Marcia Kemp sempre teve um DNA muito forte voltado para cidadania com enorme vontade de ajudar as pessoas. Começou com um trabalho voluntário no Instituto da Criança, há mais de 10 anos e em paralelo desenvolvia projetos de cidadania coorporativa. Com uma rotina de vida digna de uma executiva e empresária se dividia em inúmeras reuniões pelo Brasil e pelo mundo num cargo de extrema responsabilidade. Mas Marcia nunca deixou que a rotina afetasse outra grande paixão: a de viajar. Nada de destinos turísticos, suas preferências sempre foram roteiros exóticos que ela mesma pesquisava, curiosa por conhecer novas culturas, tradições e pessoas. Numa destas viagens, durante uma visita a tribo de Massai na África, teve um insight de compartilhar a beleza do artesanato singular, que admirava pelo mundo. Sua maior inspiração era erguer a bandeira de que a beleza oriunda da simplicidade tem um espaço e uma valorização a conquistar no universo fashion. E em seu constante interesse pela cultura dos povos, indagava diante dos belos trabalhos que conhecia, porque pessoas tinham tanta vantagem competitiva e outras nasciam desprovidas de oportunidades.
+</p>
+
+<div class="quote-author">
+  <p class="quote">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id diam sit amet orci porta eleifend sit amet malesuada nisl. Vestibulum condimentum fringilla pharetra. Praesent vel condimentum felis."</p>
+  <p class="author">Consectetur Adipiscing</p>
+</div>
+
+<p class="text">
+  Determinada a escrever outra história, resolveu arregaçar as mangas e correr atrás de seu sonho! Nasceu então a Nannacay® (Nãnãkay), nome de origem Quechua Aimara, que significa irmandade de mulheres. Muito mais do que uma marca, trata-se de um novo projeto de moda social; Creative Hands Transforming Lives. Sua missão é ajudar pessoas a desenvolver potenciais criativos, servindo de ponte entre povos que precisam de ajuda e pessoas que queiram ajudar. A primeira linha de produtos que já circula entre fashionistas, é produzida por uma comunidade de artesãs do Peru. São bolsas e objetos em junco em junco com raízes tribais que refletem o colorido, o desenho e formas vibrantes do país. Handmade e despojadas, as peças são únicas, já que um modelo nunca é igual ao outro. Todo o desenvolvimento, padronagem de cores e controle de qualidade é coordenado por Marcia, que empresta sua expertise à comunidade. Indagada sobre o perfil de quem usa Nannacay®, Marcia não hesita em responder: mulheres que gostam do belo e admiram a simplicidade da vida.
+</p>
+
+<div class="image-subtitle">
+  <img class="image" src="">
+</div>
+
+<div class="video">video</div>
+
+<div class="quote-author">
+  <p class="quote">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id diam sit amet orci porta eleifend sit amet malesuada nisl. Vestibulum condimentum fringilla pharetra. Praesent vel condimentum felis."</p>
+  <p class="author">Consectetur Adipiscing</p>
+</div>
+
+<img src="" class="full-image">
+
+
   
 <?php 
 	get_footer();
