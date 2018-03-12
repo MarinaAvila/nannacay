4c3ac33c1cb0829a07d3faa27098aa261429cae1
Marina Avila <marina@grama.cc>
2018-03-12 00:10:13 -0300

    campanhas

--- wp-content/themes/nannacay/campanhas.php
+++ wp-content/themes/nannacay/campanhas.php
@@ -4,7 +4,30 @@
   get_header();
 ?>
 
-<h1>campanhas</h1>
+<div class="cover">
+  <div class="section-info">
+    <h1 class="section-name">campanhas</h1>
+
+    <?php
+      $args = array(
+        'post_type' => 'post',
+        'posts_per_page' => 9999
+      );
+      $post_query = new WP_Query($args);
+    ?>
+
+    <ul class="campaigns">
+      <?php if ( $post_query->have_posts() ) : ?>
+        <?php while ( $post_query->have_posts() ) : $post_query->the_post(); ?>
+        <li class='jersey-card'>
+          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </li>
+        <?php endwhile; ?>
+        <?php wp_reset_postdata(); ?>
+      <?php endif; ?>
+    </ul>
+ </div>
+</div>
   
 <?php 
 	get_footer();
