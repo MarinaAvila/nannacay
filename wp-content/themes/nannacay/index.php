ff4b8830e050ff5d9777b875a3717f688dee2478
Marina Avila <marina@grama.cc>
2018-03-18 16:49:14 -0300

    Add images on home-slider

--- wp-content/themes/nannacay/index.php
+++ wp-content/themes/nannacay/index.php
@@ -4,12 +4,19 @@
   get_header();
 ?>
 
-<div class="cover" style="background-image:url('<?php echo get_field('cover') ?>"> 
-  <div class="logo" style="background-image:url(<?php echo get_field('logo') ?>"></div>
+<div class="home-slider">
+
+  <?php if ( have_rows( 'cover_slider' ) ): ?>
+    <?php while( have_rows( 'cover_slider' ) ): the_row() ?>
+
+      <div class="cover" style="background-image:url('<?php echo get_sub_field('cover') ?>"> 
+        <div class="logo" style="background-image:url(<?php echo get_sub_field('logo') ?>"></div>
+      </div>
+
+    <?php endwhile; ?>
+  <?php endif; ?>
 </div>
 
-
-  
 <?php 
 	get_footer();
 ?>
