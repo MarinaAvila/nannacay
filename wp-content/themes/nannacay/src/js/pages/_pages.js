0c8db368d9d3374f3559427c546c601474e79f91
Marina Avila <marina@grama.cc>
2018-03-20 22:57:05 -0300

    make slick work only on home

--- wp-content/themes/nannacay/src/js/pages/_pages.js
+++ wp-content/themes/nannacay/src/js/pages/_pages.js
@@ -1,5 +1,21 @@
 ;(function(){
+  
+  if (!$('.home').length) {
+    return;
+  }
 
-  console.log( 'pages' );
+  // home
+
+  $('.cover').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    fade: true,
+    infinite: true,
+    dots: false,
+    arrows: false,
+    cssEase: 'ease-in'
+  });
 
 })();
--- wp-content/themes/nannacay/src/js/script.js
+++ wp-content/themes/nannacay/src/js/script.js
@@ -61,19 +61,7 @@
   //   autoplay: true,
   // });
 
-  // home
-
-  $('.cover').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    fade: true,
-    infinite: true,
-    dots: false,
-    arrows: false,
-    cssEase: 'ease-in'
-  });
+  
 
   // clipping open modal
 
