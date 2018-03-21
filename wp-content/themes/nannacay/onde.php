4c293ea85b78d608448eb9ca92bf175decaebfb9
Marina Avila <marina@grama.cc>
2018-03-20 23:06:28 -0300

    onde mobile

--- wp-content/themes/nannacay/onde.php
+++ wp-content/themes/nannacay/onde.php
@@ -11,104 +11,109 @@
   </div>
 </div>
 
-<div class="stores-lists">
-  <div class="section">Onde</div>
-  <ul class="stores-list">
-    <li class="coutry">BRASIL</li>
-    <li>Dona Coisa - Rio de Janeiro</li>
-    <li>Via Flores - Rio de Janeiro</li>
-    <li>Dona Santa - Recife</li>
-    <li>Copacabana Palace - Rio de Janeiro</li>
-    <li>Bazaar Fashion - Curitiba</li>
-    <li>Twinset - Porto Alegre</li>
-    <li>Alameda - Campo Grande - MS</li>
-    <li>Nannai Beach Boutique - Porto de Galinhas</li>
-    <li>Iriana Basico - Santos</li>
-    <li>Tereza Ribeiro Maison - Natal</li>
-    <li>Paradoxus - Salvador</li>
-    <li>Amoreira - São Paulo</li>
-    <li>Loja Donna Moça - São Luiz</li>
-    <li>Arraial Boutique - Gramado</li>
-    <li>Mary Nuernberg – Criciúma</li>
-    <li>Donna Filó – Belém</li>
-    <li>Porta Azul Fashion Design – Bento Gonçalves</li>
-  </ul>
+<div class="stores-info"> 
 
-  <ul class="stores-list">
-    <li class="coutry">UNITED STATES</li>
-    <li>Bergdorf Goodman</li>
-    <li>Moda Operandi</li>
-    <li>Saks Fifth Avenue</li>
-    <li>Kirna Zabete</li>
-    <li>Shop Bob</li>
-    <li>Revolve</li>
-    <li>Intermix</li>
-    <li>J. Crew</li>
-    <li>Fivestory NY</li>
-    <li>Need Suply</li>
-    <li>Ace in Hole Curio</li>
-    <li>Duo NY</li>
-    <li>Haslbrook</li>
-    <li>Veronica Beard</li>
-    <li>Julies Reid</li>
-    <li>Kiki Shilling</li>
-    <li>Maris Collective Store</li>
-    <li>Nothworthy Interiors</li>
-    <li>Peri Store</li>
-    <li>Permanent Vacation</li>
-    <li>Toostsie California</li>
-  </ul>
+  <div class="stores-image"></div>
 
-  <div class="other-stores">
-
+  <div class="stores-lists">
+    <div class="section">Onde</div>
     <ul class="stores-list">
-      <li class="coutry">LONDON</li>
-      <li>Avenue 32</li>
-      <li>Fenwick</li>
+      <li class="coutry">BRASIL</li>
+      <li>Dona Coisa - Rio de Janeiro</li>
+      <li>Via Flores - Rio de Janeiro</li>
+      <li>Dona Santa - Recife</li>
+      <li>Copacabana Palace - Rio de Janeiro</li>
+      <li>Bazaar Fashion - Curitiba</li>
+      <li>Twinset - Porto Alegre</li>
+      <li>Alameda - Campo Grande - MS</li>
+      <li>Nannai Beach Boutique - Porto de Galinhas</li>
+      <li>Iriana Basico - Santos</li>
+      <li>Tereza Ribeiro Maison - Natal</li>
+      <li>Paradoxus - Salvador</li>
+      <li>Amoreira - São Paulo</li>
+      <li>Loja Donna Moça - São Luiz</li>
+      <li>Arraial Boutique - Gramado</li>
+      <li>Mary Nuernberg – Criciúma</li>
+      <li>Donna Filó – Belém</li>
+      <li>Porta Azul Fashion Design – Bento Gonçalves</li>
     </ul>
 
     <ul class="stores-list">
-      <li class="coutry">JAPAN</li>
-      <li>Dexieme Class</li>
+      <li class="coutry">UNITED STATES</li>
+      <li>Bergdorf Goodman</li>
+      <li>Moda Operandi</li>
+      <li>Saks Fifth Avenue</li>
+      <li>Kirna Zabete</li>
+      <li>Shop Bob</li>
+      <li>Revolve</li>
+      <li>Intermix</li>
+      <li>J. Crew</li>
+      <li>Fivestory NY</li>
+      <li>Need Suply</li>
+      <li>Ace in Hole Curio</li>
+      <li>Duo NY</li>
+      <li>Haslbrook</li>
+      <li>Veronica Beard</li>
+      <li>Julies Reid</li>
+      <li>Kiki Shilling</li>
+      <li>Maris Collective Store</li>
+      <li>Nothworthy Interiors</li>
+      <li>Peri Store</li>
+      <li>Permanent Vacation</li>
+      <li>Toostsie California</li>
     </ul>
 
-    <ul class="stores-list">
-      <li class="coutry">HONK KONG</li>
-      <li>Net-a-porter</li>
-      <li>Lane Crawford</li>
-    </ul>
+    <div class="other-stores">
 
-    <ul class="stores-list">
-      <li class="coutry">DUBAI</li>
-      <li>Ounass</li>
-    </ul>
+      <ul class="stores-list">
+        <li class="coutry">LONDON</li>
+        <li>Avenue 32</li>
+        <li>Fenwick</li>
+      </ul>
+
+      <ul class="stores-list">
+        <li class="coutry">JAPAN</li>
+        <li>Dexieme Class</li>
+      </ul>
+
+      <ul class="stores-list">
+        <li class="coutry">HONK KONG</li>
+        <li>Net-a-porter</li>
+        <li>Lane Crawford</li>
+      </ul>
+
+      <ul class="stores-list">
+        <li class="coutry">DUBAI</li>
+        <li>Ounass</li>
+      </ul>
+    </div>
+    
+
+    <div class="wholesale-list">
+
+      <div class="wholesale-title">ATACADO/WHOLESALE</div>
+
+      <ul class="wholesale-info">
+        <li class="store">NANNACAY Studio Store - Brasil</li>
+        <li>Av. Ataúlfo de Paiva 658/402</li>
+        <li>Leblon - Rio de Janeiro</li>
+        <li>+ 55 21 3795 7344</li>
+      </ul>
+
+      <ul class="wholesale-info">
+        <li class="store">PR EUROPE</li>
+        <li>For Press inquires</li>
+        <li>sophie@sophie-elliot.com</li>
+      </ul>
+
+      <ul class="wholesale-info">
+        <li class="store">USA</li>
+        <li>Fifty Two Showroom28th Warren St - 5th floor Loft</li>
+        <li>+1 (212) 996 5110</li>
+      </ul>
+    </div>
   </div>
   
-
-  <div class="wholesale-list">
-
-    <div class="wholesale-title">ATACADO/WHOLESALE</div>
-
-    <ul class="wholesale-info">
-      <li class="store">NANNACAY Studio Store - Brasil</li>
-      <li>Av. Ataúlfo de Paiva 658/402</li>
-      <li>Leblon - Rio de Janeiro</li>
-      <li>+ 55 21 3795 7344</li>
-    </ul>
-
-    <ul class="wholesale-info">
-      <li class="store">PR EUROPE</li>
-      <li>For Press inquires</li>
-      <li>sophie@sophie-elliot.com</li>
-    </ul>
-
-    <ul class="wholesale-info">
-      <li class="store">USA</li>
-      <li>Fifty Two Showroom28th Warren St - 5th floor Loft</li>
-      <li>+1 (212) 996 5110</li>
-    </ul>
-  </div>
-
 </div>
   
 <?php 
