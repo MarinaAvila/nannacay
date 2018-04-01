06bdc1226d7fc502ead2b816840e770f0d0d823d
Marina Avila <marina@grama.cc>
2018-04-01 15:00:58 -0300

    footer text fix

--- wp-content/themes/nannacay/footer.php
+++ wp-content/themes/nannacay/footer.php
@@ -1,8 +1,8 @@
 	
 		<ul class="footer">
-			<li><a href="<?php echo get_home_url(); ?>/#terms">Termos de Uso</a></li>
-			<li><a href="<?php echo get_home_url(); ?>/#changes">Trocas e Devoluções</a></li>
-			<li><a href="<?php echo get_home_url(); ?>/#contact">Fale Conosco</a></li>
+			<li><a href="<?php echo get_home_url(); ?>/termos/#terms">Termos de Uso</a></li>
+			<li><a href="<?php echo get_home_url(); ?>/termos/#changes">plolitica de troca</a></li>
+			<li><a href="<?php echo get_home_url(); ?>/termos/#contact">Fale Conosco</a></li>
 		</ul>
 
 		<script src="<?php echo JS; ?>script.js"></script>
