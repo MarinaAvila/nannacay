fd63f474690842bc89b12abf4577540c7b54a2b3
Marina Avila <marina@grama.cc>
2018-04-01 01:15:27 -0300

    termos style start

--- wp-content/themes/nannacay/footer.php
+++ wp-content/themes/nannacay/footer.php
@@ -1,4 +1,10 @@
 	
+		<ul class="footer">
+			<li><a href="<?php echo get_home_url(); ?>/#terms">Termos de Uso</a></li>
+			<li><a href="<?php echo get_home_url(); ?>/#changes">Trocas e Devoluções</a></li>
+			<li><a href="<?php echo get_home_url(); ?>/#contact">Fale Conosco</a></li>
+		</ul>
+
 		<script src="<?php echo JS; ?>script.js"></script>
 	</body>
 </html>
