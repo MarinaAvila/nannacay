badaf3352db1216fa2a00fb3b6c304831c0a541d
Marina Avila <marina@grama.cc>
2018-04-01 15:44:50 -0300

    links dos termos

--- wp-content/themes/nannacay/src/js/pages/_terms.js
+++ wp-content/themes/nannacay/src/js/pages/_terms.js
@@ -1,11 +1,37 @@
-;(function(){
+// ;(function(){
 
-  console.log('clippings');
+//   console.log('clippings');
+  
+//   if(window.location.href.indexOf("#terms") > -1) {
+//     scrollToTerms();
+//   }
 
-  $(".call-to-action").click(function() {
-    $('html, body').animate({
-        scrollTop: $(".clippings").offset().top
-    }, 1000);
-  });
+//   if(window.location.href.indexOf("#changes") > -1) {
+//     scrollToChanges();
+//   }
 
-})();
+//   if(window.location.href.indexOf("#contact") > -1) {
+//     scrollToContact();
+//   }
+
+// 	function scrollToTerms() {
+// 		$( 'html, body' ).animate( {
+// 			scrollTop: $('#terms').offset().top }
+// 		, 'slow');
+// 	}
+
+// 	function scrollToChanges() {
+// 		$( 'html, body' ).animate( {
+// 			scrollTop: $('#changes').offset().top }
+// 		, 'slow');
+//   }
+  
+//   function scrollToContact() {
+// 		$( 'html, body' ).animate( {
+// 			scrollTop: $('#contact').offset().top }
+// 		, 'slow');
+// 	}
+
+
+
+// })();
--- wp-content/themes/nannacay/termos.php
+++ wp-content/themes/nannacay/termos.php
@@ -7,11 +7,11 @@
 
 <div class="terms">
 
-  <h2 class="title">Termos de Uso</h2>
+  <h2 class="title" id="terms">Termos de Uso</h2>
   <?php echo the_field( 'terms' ) ?>
-  <h2 class="title">Trocas e Devoluções</h2>
+  <h2 class="title" id="changes">Trocas e Devoluções</h2>
   <?php echo the_field( 'change' ) ?>
-  <h2 class="title">Fale Conosco</h2>
+  <h2 class="title" id="contact">Fale Conosco</h2>
   <?php echo the_field( 'fale_conosco' ) ?>
 
 </div>
