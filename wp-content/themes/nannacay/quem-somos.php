2b4025951a86e07c0f1f32a1e9c3b36482ee9e68
Marina Avila <marina@grama.cc>
2018-04-01 21:01:52 -0300

    quem somos

--- wp-content/themes/nannacay/quem-somos.php
+++ wp-content/themes/nannacay/quem-somos.php
@@ -27,7 +27,7 @@
     Marcia Kemp sempre teve um DNA muito forte voltado para cidadania com enorme vontade de ajudar as pessoas. Começou com um trabalho voluntário no Instituto da Criança, há mais de 10 anos e em paralelo desenvolvia projetos de cidadania coorporativa. Com uma rotina de vida digna de uma executiva e empresária se dividia em inúmeras reuniões pelo Brasil e pelo mundo num cargo de extrema responsabilidade. Mas Marcia nunca deixou que a rotina afetasse outra grande paixão: a de viajar. Nada de destinos turísticos, suas preferências sempre foram roteiros exóticos que ela mesma pesquisava, curiosa por conhecer novas culturas, tradições e pessoas. Numa destas viagens, durante uma visita a tribo de Massai na África, teve um insight de compartilhar a beleza do artesanato singular, que admirava pelo mundo. Sua maior inspiração era erguer a bandeira de que a beleza oriunda da simplicidade tem um espaço e uma valorização a conquistar no universo fashion. E em seu constante interesse pela cultura dos povos, indagava diante dos belos trabalhos que conhecia, porque pessoas tinham tanta vantagem competitiva e outras nasciam desprovidas de oportunidades.
   </p>
 
-  <div class="quote-author mobile-quote">
+  <div class="quote-author pedro">
     <p class="quote">“uma empresa que já surgiu linda pela poesia do seu nome, ampliada pela chance de gerar renda aos artesões distantes da realidade urbana. Esse é só o início de uma história que oferece um produto belíssimo, exclusivo e repleto de amor.”</p>
     <p class="author">Pedro Werneck - Presidente do Instituto da Criança</p>
   </div>
@@ -40,19 +40,11 @@
     Determinada a escrever outra história, resolveu arregaçar as mangas e correr atrás de seu sonho! Nasceu então a Nannacay® (Nãnãkay), nome de origem Quechua Aimara, que significa irmandade de mulheres. Muito mais do que uma marca, trata-se de um novo projeto de moda social; Creative Hands Transforming Lives. Sua missão é ajudar pessoas a desenvolver potenciais criativos, servindo de ponte entre povos que precisam de ajuda e pessoas que queiram ajudar. A primeira linha de produtos que já circula entre fashionistas, é produzida por uma comunidade de artesãs do Peru. São bolsas e objetos em junco em junco com raízes tribais que refletem o colorido, o desenho e formas vibrantes do país. Handmade e despojadas, as peças são únicas, já que um modelo nunca é igual ao outro. Todo o desenvolvimento, padronagem de cores e controle de qualidade é coordenado por Marcia, que empresta sua expertise à comunidade. Indagada sobre o perfil de quem usa Nannacay®, Marcia não hesita em responder: mulheres que gostam do belo e admiram a simplicidade da vida.
   </p>
 
-  <div class="collumn-quotes">
-
-    <div class="quote-author desktop-quote">
-      <p class="quote">“uma empresa que já surgiu linda pela poesia do seu nome, ampliada pela chance de gerar renda aos artesões distantes da realidade urbana. Esse é só o início de uma história que oferece um produto belíssimo, exclusivo e repleto de amor.”</p>
-      <p class="author">Pedro Werneck - Presidente do Instituto da Criança</p>
-    </div>
-
-    <div class="quote-author">
-      <p class="quote">“Acompanho de perto a intensa dedicação da Marcia Kemp em fazer deste projeto social a transformação na vida dessas incríveis artesãs. O resultado de tudo isso é um produto diferenciado, único e de muito bom gosto.”</p>
-      <p class="author">Lenny Niemyer</p>
-    </div>
-
+  <div class="quote-author lenny">
+    <p class="quote">“Acompanho de perto a intensa dedicação da Marcia Kemp em fazer deste projeto social a transformação na vida dessas incríveis artesãs. O resultado de tudo isso é um produto diferenciado, único e de muito bom gosto.”</p>
+    <p class="author">Lenny Niemyer</p>
   </div>
+
 
 </div>
 
