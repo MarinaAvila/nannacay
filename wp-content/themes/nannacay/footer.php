fef232051cea4abcc77ea1dd161165f76decc6df
Marina Avila <marina@grama.cc>
2018-04-29 19:05:16 -0300

    footer

--- wp-content/themes/nannacay/footer.php
+++ wp-content/themes/nannacay/footer.php
@@ -1,9 +1,16 @@
 	
-		<ul class="footer">
-			<li><a href="<?php echo get_home_url(); ?>/termos/#terms">Termos de Uso</a></li>
-			<li><a href="<?php echo get_home_url(); ?>/termos/#changes">política de troca</a></li>
-			<li><a href="<?php echo get_home_url(); ?>/termos/#contact">Fale Conosco</a></li>
-		</ul>
+		<div class="footer">
+			<ul class="left">
+				<li><a href="<?php echo get_home_url(); ?>/termos/#terms">Termos de Uso</a></li>
+				<li><a href="<?php echo get_home_url(); ?>/termos/#changes">política de troca</a></li>
+				<li><a href="<?php echo get_home_url(); ?>/termos/#contact">Fale Conosco</a></li>
+			</ul>
+			<ul class="right">
+				<li><a href="https://www.instagram.com/nannacay/">Instagram</a></li>
+				<li><a href="https://www.facebook.com/nannacay.creativehands/">Facebook</a></li>
+				<li class="ltda">2018 Nannacay LTDA</li>
+			</ul>
+		</div>
 
 		<script src="<?php echo JS; ?>script.js"></script>
 	</body>
