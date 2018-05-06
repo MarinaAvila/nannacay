6913820eca09326e7812fa6676f2c10b900d5f2f
Marina Avila <marina@grama.cc>
2018-05-05 22:47:29 -0300

    add footer link

--- wp-content/themes/nannacay/footer.php
+++ wp-content/themes/nannacay/footer.php
@@ -4,6 +4,7 @@
 				<li><a href="<?php echo get_home_url(); ?>/termos/#terms">Termos de Uso</a></li>
 				<li><a href="<?php echo get_home_url(); ?>/termos/#changes">política de troca</a></li>
 				<li><a href="<?php echo get_home_url(); ?>/termos/#contact">Fale Conosco</a></li>
+				<li><a href="https://shop.nannacay.com/pages/newsletter" target="_blank">newsletter</a></li>
 			</ul>
 			<ul class="right">
 				<li><a href="https://www.instagram.com/nannacay/">Instagram</a></li>
